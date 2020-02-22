d05d4083a0d051e6166ff3c109ff1c922934d312
jdcarlson07 <56281452+jdcarlson07@users.noreply.github.com>
2020-02-22 12:56:29 -0600

    Update CardInfoScreen.py

--- CardInfoScreen.py
+++ CardInfoScreen.py
@@ -8,21 +8,35 @@ class Window(Frame):
         self.init_window()
 
     def init_window(self):
+
+        #title
+        self.master.title("GUI")
+        
+        #setting up labels and button
         Label(self.master, text = "Card Number: ").grid(row = 0)
         Label(self.master, text = "Expiration Date (mmyy): ").grid(row = 1)
         Label(self.master, text = "CVV: ").grid(row = 2)
         Label(self.master, text = "Zip Code: ").grid(row = 3)
+        enter = Button(root, text = "Enter")
+        enter.grid(row = 4, column = 1)
+
+        #defining entry variables
         cardNum = Entry(self.master)
         expDate = Entry(self.master)
         cvv = Entry(self.master)
         zipCode = Entry(self.master)
+
+        #putting those entry variables into the grid
         cardNum.grid(row = 0, column = 1)
         expDate.grid(row = 1, column = 1)
         cvv.grid(row = 2, column = 1)
         zipCode.grid(row = 3, column = 1)
 
 
-root = Tk()
+
+
+
+root = Tk(screenName = "Card Info")
 root.geometry("800x600")
 app = Window(root)
 root.mainloop()
