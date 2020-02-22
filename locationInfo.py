03173edf250804d40bc8ef3a245602d5a36e718b
Kris Clancy <kristopher.clancy@cuw.edu>
2020-02-22 12:32:27 -0600

    locationInfoPage
    first test

---
+++ locationInfo.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+#main function
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master=None)
+        self.master = master
+        self.init_window()
+    def init_window(self):
+
+        #title of master widget
+        self.master.title("GUI")
+
+        Label(self.master, text = "Enter Location").grid(row=0)
+        Label(self.master, text = "Address: ").grid(row=1)
+        Label(self.master, text = "City: ").grid(row=2)
+        Label(self.master, text = "State: ").grid(row=3)
+        Label(self.master, text = "ZIP: ").grid(row=4)
+
+        
+        address = Entry(self.master)
+        city = Entry(self.master)
+        state = Entry(self.master)
+        zipcode = Entry(self.master)
+
+        
+        address.grid(row = 1, column = 1)
+        city.grid(row = 2, column = 1)
+        state.grid(row = 3, column = 1)
+        zipcode.grid(row = 4, column = 1)
+
+        root.geometry("800x600")
+        
+root = Tk()
+
+
+app = Window(root)
+root.mainloop()
