2c226fc81f11952e3a802e008d6e3640ffae8afc
jdcarlson07 <56281452+jdcarlson07@users.noreply.github.com>
2020-02-22 14:28:49 -0600

    Update CardInfoScreen.py

--- CardInfoScreen.py
+++ CardInfoScreen.py
@@ -9,34 +9,37 @@ class Window(Frame):
 
     def init_window(self):
 
+        
+
         #title
-        self.master.title("GUI")
+        self.master.title("Payment Information")
         
         #setting up labels and button
         Label(self.master, text = "Card Number: ").grid(row = 0)
         Label(self.master, text = "Expiration Date (mmyy): ").grid(row = 1)
         Label(self.master, text = "CVV: ").grid(row = 2)
         Label(self.master, text = "Zip Code: ").grid(row = 3)
-        enter = Button(root, text = "Enter")
-        enter.grid(row = 4, column = 1)
-
-        #defining entry variables
-        cardNum = Entry(self.master)
-        expDate = Entry(self.master)
-        cvv = Entry(self.master)
-        zipCode = Entry(self.master)
-
-        #putting those entry variables into the grid
-        cardNum.grid(row = 0, column = 1)
-        expDate.grid(row = 1, column = 1)
-        cvv.grid(row = 2, column = 1)
-        zipCode.grid(row = 3, column = 1)
-
-    def nextAndEnter():
         
 
+        #defining entry variables
+        cardNumInput = Entry(self.master)
+        expDateInput = Entry(self.master)
+        cvvInput = Entry(self.master)
+        zipCodeInput = Entry(self.master)
 
+        def test():
+            print(cardNumInput.get())
+            print(expDateInput.get())
+            print(cvvInput.get())
+            print(zipCodeInput.get())
 
+        #populating the grid
+        cardNumInput.grid(row = 0, column = 1)
+        expDateInput.grid(row = 1, column = 1)
+        cvvInput.grid(row = 2, column = 1)
+        zipCodeInput.grid(row = 3, column = 1)
+        enter = Button(root, text = "Enter", command = test)
+        enter.grid(row = 4, column = 1)
 
 root = Tk(screenName = "Card Info")
 root.geometry("800x600")
